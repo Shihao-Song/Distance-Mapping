e33e1a04358c1b8150b89a9162d246397d38e726
shihao-song <ss3695@drexel.edu>
2018-10-31 19:37:23 -0400

    Changed to C++ STL

--- src/maurer_openmp/maurer.cpp
+++ src/maurer_openmp/maurer.cpp
@@ -1,56 +1,54 @@
 #include "maurer.h"
 
-void maurerFT(uchar *vol, float *sp2, \
-	int height, int width, int depth, \
+void maurerFT(unsigned char *vol, 
+	float *sp2,
+	int height, int width, int depth,
 	double *output)
 {
 	int dim;
 
 	for (dim = 1; dim <= 3; dim++)
 	{
-		VoronoiFT(dim, vol, sp2, height, width, depth, output);
+		voronoiFT(dim, vol, sp2, height, width, depth, output);
 	}
 }
 
-void VoronoiFT(int dim, \
-	uchar *vol, float *sp2, \
-	int height, int width, int depth, \
+void voronoiFT(int dim,
+	unsigned char *vol, 
+	float *sp2,
+	int height, int width, int depth,
 	double *output)
 {
 	switch (dim)
 	{
 		case 1:
-			RunVoronoiFT1D(vol, sp2, height, width, depth, output);
+			runVoronoiFT1D(vol, sp2, height, width, depth, output);
 			break;
 		case 2:
-			RunVoronoiFT2D(sp2, height, width, depth, output);
+			runVoronoiFT2D(sp2, height, width, depth, output);
 			break;
 		case 3:
-			RunVoronoiFT3D(sp2, height, width, depth, output);
+			runVoronoiFT3D(sp2, height, width, depth, output);
 			break;
 		default:
 			break;
 	}
 }
 
-void RunVoronoiFT1D(uchar *vol, float *sp2, \
-		int height, int width, int depth,\
+void runVoronoiFT1D(unsigned char *vol, float *sp2,
+		int height, int width, int depth,
 		double *output)
 {
+	// GNodes
+	deque<vector<int>> g_nodes;
 
-	GNodes g;
-	
 	// Distance between slices
 	int slice_stride = height * width;
 
 	int k;
 
-	#pragma omp parallel shared(vol, sp2, output) private(g,k)
+	#pragma omp parallel shared(vol, sp2, output) private(g_nodes,k)
 	{
-	g.size = 0;
-        g.max_size = width;
-        g.stack = (node *)malloc(g.max_size * sizeof(node));
-
 	#pragma omp for
 	for (k = 0; k < depth; k++)
 	{
@@ -62,11 +60,13 @@ void RunVoronoiFT1D(uchar *vol, float *sp2, \
 			{
 				if (vol[k * slice_stride + i * width + j] != 0)
 				{
-					push(&g, i, j, k);
+					vector<int> fv {i, j, k};
+					
+					g_nodes.push_back(fv);
 				}
 			}
 
-			if (g.size == 0)
+			if (g_nodes.size() == 0)
 			{
 				continue;
 			}
@@ -75,28 +75,29 @@ void RunVoronoiFT1D(uchar *vol, float *sp2, \
 			for (j = 0; j < width; j++)
 			{
 				int ite = 0;
-				while ((ite < (g.size - 1)) && \
-					(ED(sp2, i, j, k, &(g.stack[ite])) > \
-					ED(sp2, i, j, k, &(g.stack[ite+1]))))
+				while ((ite < (g_nodes.size() - 1)) &&
+					(ED(sp2, i, j, k, g_nodes[ite]) >
+					ED(sp2, i, j, k, g_nodes[ite+1])))
 				{
 					ite++;
 				}
 
-				output[k * slice_stride + i * width + j] = \
-					double(g.stack[ite].fv_pos[2] * slice_stride + \
-					g.stack[ite].fv_pos[0] * width + \
-					g.stack[ite].fv_pos[1]);
+				output[k * slice_stride + i * width + j] =
+					double(g_nodes[ite][2] * slice_stride +
+					g_nodes[ite][0] * width +
+					g_nodes[ite][1]);
 			}
-			g.size = 0;
+			g_nodes.clear();
 		}
 	}
-	free(g.stack);
 	}
 }
 
-void RunVoronoiFT2D(float *sp2, int height, int width, int depth, double *vol)
+void runVoronoiFT2D(float *sp2, 
+		int height, int width, int depth, 
+		double *vol)
 {
-	GNodes g;
+	deque<vector<int>> g_nodes;
 	
 	int Rd[3];
 	int w[3];
@@ -105,12 +106,8 @@ void RunVoronoiFT2D(float *sp2, int height, int width, int depth, double *vol)
 	int slice_stride = height * width;
 
 	int k;
-	#pragma omp parallel shared(sp2, vol) private(g, Rd, w, k)
+	#pragma omp parallel shared(sp2, vol) private(g_nodes, Rd, w, k)
     	{
-	g.size = 0;
-	g.max_size = height;
-	g.stack = (node *)malloc(g.max_size * sizeof(node));
-
 	#pragma omp for
 	for (k = 0; k < depth; k++)
 	{
@@ -122,18 +119,20 @@ void RunVoronoiFT2D(float *sp2, int height, int width, int depth, double *vol)
 			{
 				if (vol[k * slice_stride + i * width + j] != -1.0)
 				{
-					int fv_k = int(vol[k * slice_stride + i * width + j]) \
+					int fv_k = int(vol[k * slice_stride + i * width + j])
 							/ slice_stride;
 					
-					int fv_i = (int(vol[k * slice_stride + i * width + j]) \
+					int fv_i = (int(vol[k * slice_stride + i * width + j])
 							% slice_stride) / width;
 					
-					int fv_j = (int(vol[k * slice_stride + i * width + j]) \
+					int fv_j = (int(vol[k * slice_stride + i * width + j])
 							% slice_stride) % width;
 					
-					if(g.size < 2)
+					if(g_nodes.size() < 2)
 					{
-						push(&g, fv_i, fv_j, fv_k);
+						vector<int> fv {fv_i, fv_j, fv_k};
+
+						g_nodes.push_back(fv);
 					}
 					else
 					{
@@ -145,17 +144,21 @@ void RunVoronoiFT2D(float *sp2, int height, int width, int depth, double *vol)
 						Rd[1] = j;
 						Rd[2] = k;
 
-						while (g.size >= 2 && removeFT2D(sp2, &g, w, Rd))
+						while (g_nodes.size() >= 2 && 
+							removeFT2D(sp2, g_nodes, w, Rd))
 						{
-							pop(&g);
+							g_nodes.pop_back();
 						}
 
-						push(&g, fv_i, fv_j, fv_k);
+						vector<int> fv {fv_i, fv_j, fv_k};
+
+						g_nodes.push_back(fv);
+
 					}
 				}
 			}
 
-			if (g.size == 0)
+			if (g_nodes.size() == 0)
 			{
 				continue;
 			}
@@ -167,9 +170,9 @@ void RunVoronoiFT2D(float *sp2, int height, int width, int depth, double *vol)
 				int minIndex = -1;
 
 				int ite = 0;
-				while(ite < g.size)
+				while(ite < g_nodes.size())
 				{
-					double tempDist = ED(sp2, i, j, k, &(g.stack[ite]));
+					double tempDist = ED(sp2, i, j, k, g_nodes[ite]);
 
 					if(tempDist < minDist)
 					{
@@ -180,20 +183,19 @@ void RunVoronoiFT2D(float *sp2, int height, int width, int depth, double *vol)
 				}
 
 				vol[k * slice_stride + i * width + j] = \
-					double(g.stack[minIndex].fv_pos[2] * slice_stride + \
-					g.stack[minIndex].fv_pos[0] * width + \
-					g.stack[minIndex].fv_pos[1]);
+					double(g_nodes[minIndex][2] * slice_stride +
+					g_nodes[minIndex][0] * width +
+					g_nodes[minIndex][1]);
 			}
-			g.size = 0;
+			g_nodes.clear();
 		}
 	}
-	free(g.stack);
 	}
 }
 
-void RunVoronoiFT3D(float *sp2, int height, int width, int depth, double *vol)
+void runVoronoiFT3D(float *sp2, int height, int width, int depth, double *vol)
 {
-	GNodes g;
+	deque<vector<int>> g_nodes;
 
 	int Rd[3];
 	int w[3];
@@ -202,12 +204,8 @@ void RunVoronoiFT3D(float *sp2, int height, int width, int depth, double *vol)
 	int slice_stride = height * width;
 
 	int i;
-	#pragma omp parallel shared(sp2, vol) private(g, Rd, w, i)
+	#pragma omp parallel shared(sp2, vol) private(g_nodes, Rd, w, i)
     	{
-	g.size = 0;
-	g.max_size = depth;
-	g.stack = (node *)malloc(g.max_size * sizeof(node));
-
 	#pragma omp for
 	for (i = 0; i < height; i++)
 	{
@@ -219,18 +217,20 @@ void RunVoronoiFT3D(float *sp2, int height, int width, int depth, double *vol)
 			{
 				if (vol[k * slice_stride + i * width + j] != -1.0)
 				{
-					int fv_k = int(vol[k * slice_stride + i * width + j]) \
+					int fv_k = int(vol[k * slice_stride + i * width + j])
 							/ slice_stride;
 
-					int fv_i = (int(vol[k * slice_stride + i * width + j]) \
+					int fv_i = (int(vol[k * slice_stride + i * width + j])
 							% slice_stride) / width;
 
-					int fv_j = (int(vol[k * slice_stride + i * width + j]) \
+					int fv_j = (int(vol[k * slice_stride + i * width + j])
 							% slice_stride) % width;
 					
-					if(g.size < 2)
+					if(g_nodes.size() < 2)
 					{
-						push(&g, fv_i, fv_j, fv_k);
+						vector<int> fv {fv_i, fv_j, fv_k};
+
+						g_nodes.push_back(fv);
 					}
 					else
 					{
@@ -242,17 +242,21 @@ void RunVoronoiFT3D(float *sp2, int height, int width, int depth, double *vol)
 						Rd[1] = j;
 						Rd[2] = k;
 
-						while (g.size >= 2 && removeFT3D(sp2, &g, w, Rd))
+						while (g_nodes.size() >= 2 && 
+							removeFT3D(sp2, g_nodes, w, Rd))
 						{
-							pop(&g);
+							g_nodes.pop_back();
 						}
 
-						push(&g, fv_i, fv_j, fv_k);
+						vector<int> fv {fv_i, fv_j, fv_k};
+
+						g_nodes.push_back(fv);
+
 					}
 				}
 			}
-
-			if (g.size == 0)
+				
+			if (g_nodes.size() == 0)
 			{
 				continue;
 			}
@@ -264,9 +268,9 @@ void RunVoronoiFT3D(float *sp2, int height, int width, int depth, double *vol)
 				int minIndex = -1;
 
 				int ite = 0;
-				while(ite < g.size)
+				while(ite < g_nodes.size())
 				{
-					double tempDist = ED(sp2, i, j, k, &(g.stack[ite]));
+					double tempDist = ED(sp2, i, j, k, g_nodes[ite]);
 
 					if(tempDist < minDist)
 					{
@@ -277,24 +281,23 @@ void RunVoronoiFT3D(float *sp2, int height, int width, int depth, double *vol)
 				}
 
 				vol[k * slice_stride + i * width + j] = \
-					double(g.stack[minIndex].fv_pos[2] * slice_stride + \
-					g.stack[minIndex].fv_pos[0] * width + \
-					g.stack[minIndex].fv_pos[1]);
+					double(g_nodes[minIndex][2] * slice_stride + \
+					g_nodes[minIndex][0] * width + \
+					g_nodes[minIndex][1]);
 			}
-			g.size = 0;
+			g_nodes.clear();
 		}
 	}
-	free(g.stack);
 	}
 }
 
-int removeFT2D(float *sp2, GNodes *g, int *w, int *Rd)
+int removeFT2D(float *sp2, deque<vector<int>> &g_nodes, int *w, int *Rd)
 {
-	node u = g->stack[g->size - 2];
-	node v = g->stack[g->size - 1];
+	vector<int> u = g_nodes[g_nodes.size() - 2];
+	vector<int> v = g_nodes[g_nodes.size() - 1];
 
-	double a = (v.fv_pos[0] - u.fv_pos[0]) * sqrt(sp2[0]);
-	double b = (w[0] - v.fv_pos[0]) * sqrt(sp2[0]);
+	double a = (v[0] - u[0]) * sqrt(sp2[0]);
+	double b = (w[0] - v[0]) * sqrt(sp2[0]);
 	double c = a + b;
 
 	double vRd = 0.0;
@@ -304,8 +307,8 @@ int removeFT2D(float *sp2, GNodes *g, int *w, int *Rd)
 	int i = 1;
 	for (i; i < 3; i++)
 	{
-		vRd += (v.fv_pos[i] - Rd[i]) * (v.fv_pos[i] - Rd[i]) * sp2[i];
-		uRd += (u.fv_pos[i] - Rd[i]) * (u.fv_pos[i] - Rd[i]) * sp2[i];
+		vRd += (v[i] - Rd[i]) * (v[i] - Rd[i]) * sp2[i];
+		uRd += (u[i] - Rd[i]) * (u[i] - Rd[i]) * sp2[i];
 		wRd += (w[i] - Rd[i]) * (w[i] - Rd[i]) * sp2[i];
 	}
 
@@ -313,13 +316,13 @@ int removeFT2D(float *sp2, GNodes *g, int *w, int *Rd)
 
 }
 
-int removeFT3D(float *sp2, GNodes *g, int *w, int *Rd)
+int removeFT3D(float *sp2, deque<vector<int>> &g_nodes, int *w, int *Rd)
 {
-	node u = g->stack[g->size - 2];
-	node v = g->stack[g->size - 1];
+	vector<int> u = g_nodes[g_nodes.size() - 2];
+	vector<int> v = g_nodes[g_nodes.size() - 1];
 
-	double a = (v.fv_pos[2] - u.fv_pos[2]) * sqrt(sp2[2]);
-	double b = (w[2] - v.fv_pos[2]) * sqrt(sp2[2]);
+	double a = (v[2] - u[2]) * sqrt(sp2[2]);
+	double b = (w[2] - v[2]) * sqrt(sp2[2]);
 	double c = a + b;
 
 	double vRd = 0;
@@ -329,23 +332,23 @@ int removeFT3D(float *sp2, GNodes *g, int *w, int *Rd)
 	int i = 0;
 	for (i; i < 2; i++)
 	{
-		vRd += (v.fv_pos[i] - Rd[i]) * (v.fv_pos[i] - Rd[i]) * sp2[i];
-		uRd += (u.fv_pos[i] - Rd[i]) * (u.fv_pos[i] - Rd[i]) * sp2[i];
+		vRd += (v[i] - Rd[i]) * (v[i] - Rd[i]) * sp2[i];
+		uRd += (u[i] - Rd[i]) * (u[i] - Rd[i]) * sp2[i];
 		wRd += (w[i] - Rd[i]) * (w[i] - Rd[i]) * sp2[i];
 	}
 
 	return (c * vRd - b * uRd - a * wRd - a * b * c > 0.0);
 }
 
-double ED(float *sp2, \
-	int vol_i, int vol_j, int vol_k, \
-	node *fv)
+double ED(float *sp2, 
+		int vol_i, int vol_j, int vol_k, 
+		vector<int> &fv)
 {
 	double temp = 0;
 
-	temp = (fv->fv_pos[0] - vol_i) * (fv->fv_pos[0] - vol_i) * sp2[0] +\
-			(fv->fv_pos[1] - vol_j) * (fv->fv_pos[1] - vol_j) * sp2[1] +\
-			(fv->fv_pos[2] - vol_k) * (fv->fv_pos[2] - vol_k) * sp2[2];
+        temp = (fv[0] - vol_i) * (fv[0] - vol_i) * sp2[0] +\
+                        (fv[1] - vol_j) * (fv[1] - vol_j) * sp2[1] +\
+                        (fv[2] - vol_k) * (fv[2] - vol_k) * sp2[2];
 
-	return sqrt(temp);
+        return sqrt(temp);
 }
--- src/maurer_openmp/maurer.h
+++ src/maurer_openmp/maurer.h
@@ -1,32 +1,32 @@
 #ifndef _MAURER_H_
 #define _MAURER_H_
 
-#include <math.h>
-#include <float.h>
+#include <cmath>
+#include <cfloat>
+#include <deque>
+#include <vector>
 
 #include "omp.h"
 
-#include "gnodes.h"
-
-typedef unsigned char uchar;
+using namespace std;
 
 /*
 	maurerFT: takes the raw volume and generates its FT (Feature Transformation)
 */
-void maurerFT(uchar *, float *, int, int, int, double *);
+void maurerFT(unsigned char *, float *, int, int, int, double *);
 
 /*
 	Helper functions, the following functions are the building blocks of maurerFT()
 */
-void VoronoiFT(int, uchar *, float *, int, int, int, double *);
+void voronoiFT(int, unsigned char *, float *, int, int, int, double *);
 
-void RunVoronoiFT1D(uchar *, float *, int, int, int, double *);
-void RunVoronoiFT2D(float *, int, int, int, double *);
-void RunVoronoiFT3D(float *, int, int, int, double *);
+void runVoronoiFT1D(unsigned char *, float *, int, int, int, double *);
+void runVoronoiFT2D(float *, int, int, int, double *);
+void runVoronoiFT3D(float *, int, int, int, double *);
 
-int removeFT2D(float *, GNodes *g, int *w, int *Rd);
-int removeFT3D(float *, GNodes *g, int *w, int *Rd);
+int removeFT2D(float *, deque<vector<int>> &g_nodes, int *w, int *Rd);
+int removeFT3D(float *, deque<vector<int>> &g_nodes, int *w, int *Rd);
 
-double ED(float *, int, int, int, node *); // Calculate Euclidean Distance
+double ED(float *, int, int, int, vector<int> &fv);// Calculate Euclidean Distance
 
 #endif
